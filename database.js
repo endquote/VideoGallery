a47a7128703a4cc21f36a3aac47e0542e48184a8
Josh Santangelo <josh@endquote.com>
2017-02-02 17:30:56 -0800

    ensure selection, cleanup throughout

--- database.js
+++ database.js
@@ -30,37 +30,67 @@ class Database {
     this.Video = mongoose.model('video', videoSchema);
   }
 
+  // Get all of the videos in the collection.
   getVideos() {
     return this.Video.find();
   }
 
+  // Add a video to the collection by URL.
   addVideo(url) {
     return new this.Video({ url, selected: false })
       .save()
       .then((doc) => {
         if (doc) {
           this.emit('videoAdded', doc);
+          this.ensureSelection();
+        }
+      }).catch((err) => {
+        // Already exists
+        if (err.code !== 11000) {
+          throw err;
         }
       });
   }
 
+  // Remove a video from the collection by URL.
   removeVideo(url) {
     return this.Video.findOneAndRemove({ url })
       .then((doc) => {
         if (doc) {
           this.emit('videoRemoved', doc);
+          this.ensureSelection();
         }
       });
   }
 
+  // Select a video from the collection by URL.
   selectVideo(url) {
     return this.Video.updateMany({ selected: true }, { selected: false })
       .then(() => this.Video.findOneAndUpdate({ url }, { selected: true }, { new: true }))
       .then((doc) => {
+        this.emit('videoSelected', doc);
+      });
+  }
+
+  // Get the currently selected video.
+  selectedVideo() {
+    return this.Video.findOne({ selected: true });
+  }
+
+  // Make sure a video is selected, doesn't matter which.
+  ensureSelection() {
+    this.selectedVideo().then((selectedDoc) => {
+      if (selectedDoc) {
+        return;
+      }
+      this.Video.findOne().then((doc) => {
         if (doc) {
-          this.emit('videoSelected', doc);
+          this.selectVideo(doc.url);
+        } else {
+          this.emit('videoSelected', null);
         }
       });
+    });
   }
 }
 
--- webServer.js
+++ webServer.js
@@ -4,33 +4,42 @@ const path = require('path');
 
 class WebServer {
   static init(database, port = 8080) {
+    // Set up web server.
     const app = express();
     const httpServer = WebServer.httpServer = http.Server(app);
-
     httpServer.listen(port);
 
+    // Routes for static content.
     app.get('/', (req, res) => {
-      res.sendfile(path.join(__dirname, 'client', 'index.html'));
+      res.sendfile(path.join(__dirname, 'client', 'player.html'));
     });
+    app.get('/admin', (req, res) => {
+      res.sendfile(path.join(__dirname, 'client', 'admin.html'));
+    });
+    app.use(express.static(path.join(__dirname, 'client')));
 
+    // Get all videos.
     app.get('/videos', (req, res) => {
       database.getVideos()
         .then(result => res.json(result));
     });
 
+    // Post to add a video.
     app.post('/video', (req, res) => {
       database.addVideo(req.query.url)
         .then(() => res.sendStatus(200))
         .catch(() => res.sendStatus(500));
     });
 
+    // Delete to delete a video.
     app.delete('/video', (req, res) => {
       database.removeVideo(req.query.url)
         .then(() => res.sendStatus(200))
         .catch(() => res.sendStatus(500));
     });
 
-    app.put('/selectVideo', (req, res) => {
+    // Put to select a video.
+    app.put('/video', (req, res) => {
       database.selectVideo(req.query.url)
         .then(() => res.sendStatus(200))
         .catch(() => res.sendStatus(500));
