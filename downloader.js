4991fd5d85d2138845eeeaeaa5bf6eb1b6964b07
Josh Santangelo <josh@endquote.com>
2017-02-04 18:51:12 -0800

    file downloader

---
+++ downloader.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const fs = require('fs-extra');
+const childProcess = require('child_process');
+
+class Downloader {
+  static init(database, target) {
+    this.database = database;
+    this.target = target || path.join(__dirname, 'downloads');
+
+    // https://github.com/rg3/youtube-dl/blob/master/README.md
+    this.infoCmd = 'youtube-dl --dump-json --playlist-items 1';
+    this.downloadCmd = 'youtube-dl --write-thumbnail --playlist-items 1 -o';
+
+    fs.ensureDirSync(this.target);
+    database.on('videoAdded', doc => this.addVideo(doc));
+    database.on('videoRemoved', doc => this.removeVideo(doc));
+  }
+
+  static addVideo(doc) {
+    this._getVideoInfo(doc);
+  }
+
+  static _getVideoInfo(doc) {
+    childProcess.exec(`${this.infoCmd} ${doc.url}`,
+      (err, stdout, stderr) => this._onVideoInfo(err, stdout, stderr, doc));
+  }
+
+  static _onVideoInfo(err, stdout, stderr, doc) {
+    if (err || stderr) {
+      this.database.removeVideo(doc.url);
+      return;
+    }
+
+    const info = JSON.parse(stdout);
+
+    doc.created = new Date(
+      parseInt(info.upload_date.substr(0, 4), 10),
+      parseInt(info.upload_date.substr(4, 2), 10) - 1,
+      parseInt(info.upload_date.substr(6, 2), 10));
+    doc.author = info.uploader;
+    doc.title = info.title;
+    doc.description = info.description;
+    doc.file = doc.id + path.parse(info._filename).ext;
+    doc.thumbnail = `${doc.id}.jpg`;
+    doc.url = info.webpage_url;
+    doc.save()
+      .then(() => this._downloadVideo(doc));
+  }
+
+  static _downloadVideo(doc) {
+    childProcess.exec(`${this.downloadCmd} "${this.target}/${doc.id}" ${doc.url}`,
+      (err, stdout, stderr) => this._onVideoLoaded(err, stdout, stderr, doc));
+  }
+
+  static _onVideoLoaded(err, stdout, stderr, doc) {
+    if (err || stderr) {
+      this.database.removeVideo(doc.url);
+      return;
+    }
+    doc.loaded = true;
+    doc.save();
+  }
+
+  static removeVideo(doc) {
+    try {
+      fs.unlinkSync(path.join(this.target, doc.file));
+      fs.unlinkSync(path.join(this.target, doc.thumbnail));
+    } catch (e) {
+      // ignore
+    }
+  }
+}
+
+module.exports = Downloader;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Database = require('./database');
 const WebServer = require('./webServer');
 const SocketServer = require('./socketServer');
+const Downloader = require('./downloader');
 
 const database = new Database();
 WebServer.init(database);
 SocketServer.init(WebServer.httpServer, database);
+Downloader.init(database);
