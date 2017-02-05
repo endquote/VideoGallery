c84febeb5d2232467ee9b7d80cc5b2245364df38
Josh Santangelo <josh@endquote.com>
2017-02-04 19:06:05 -0800

    documenting arguments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const WebServer = require('./webServer');
 const SocketServer = require('./socketServer');
 const Downloader = require('./downloader');
 
-const database = new Database();
-WebServer.init(database);
+const database = new Database(); // can pass connection string
+WebServer.init(database); // can pass port
 SocketServer.init(WebServer.httpServer, database);
-Downloader.init(database);
+Downloader.init(database); // can pass target dir
