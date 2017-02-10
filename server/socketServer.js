197225305b2339d102fb7b3c7337b2c13cfc4d84
Josh Santangelo <josh@endquote.com>
2017-02-10 00:29:45 -0800

    don't emit vidoes on socket connection, add static emit method

--- server/socketServer.js
+++ server/socketServer.js
@@ -5,11 +5,11 @@ const WebServer = require('./webServer');
 
 class SocketServer {
   static init() {
-    const io = socketio(WebServer.httpServer);
+    this.io = socketio(WebServer.httpServer);
+    console.log('Socket server listening');
 
-    io.on('connection', (socket) => {
-      // Send the full list when connected.
-      Database.getVideos().then(doc => socket.emit('videos', doc));
+    this.io.on('connection', (socket) => {
+      console.log('Socket connected');
 
       // Send events to the client when anything on the database changes.
       Database.on('videoAdded', doc => socket.emit('videoAdded', doc));
@@ -17,6 +17,10 @@ class SocketServer {
       Database.on('videoSelected', doc => socket.emit('videoSelected', doc));
       Database.on('videoUpdated', doc => socket.emit('videoUpdated', doc));
     });
+  }
+
+  static emit(msg, data) {
+    this.io.emit(msg, data);
   }
 }
 
