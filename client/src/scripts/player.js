1931237c3734f8e77f2674bcd9bfea56e3af60f2
Josh Santangelo <josh@endquote.com>
2017-02-12 11:29:22 -0800

    work on player

--- client/src/scripts/player.js
+++ client/src/scripts/player.js
@@ -6,9 +6,47 @@ Vue.use(VueResource);
 
 class PlayerPage {
   static init() {
+    Vue.resource('/videos')
+      .get()
+      .catch(() => window.alert('Couldn\'t load data. Is the database server running?'))
+      .then((res) => {
+        const videos = res.body;
+        this._buildApp(videos);
+        this._getUpdates(videos);
+      });
+  }
+
+  static _buildApp(videos) {
+
+    this.app = new Vue({
+      el: '#app',
+
+      data: {
+        video: videos.find(v => v.selected) || {},
+      },
+
+      components: {
+
+        'video-player': {
+          props: ['video'],
+        },
+
+        'video-info': {
+          props: ['video'],
+        },
+      },
+    });
+  }
+
+  static _getUpdates(videos) {
     const socket = io.connect();
     socket.on('controller', (data) => {
       console.log(data);
+    });
+
+    // Update the selected video.
+    socket.on('videoSelected', (video) => {
+      this.app.video = video || {};
     });
   }
 }
