a6d5acc3edd1b4c90284dc4529bc5dd76545d45a
Josh Santangelo <josh@endquote.com>
2017-02-12 19:21:01 -0800

    separate library/app code

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,7 +1,7 @@
 /* eslint arrow-body-style: "off" */
 
 const gulp = require('gulp');
-const browserify = require('gulp-browserify');
+const browserify = require('browserify');
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
 const sequence = require('gulp-sequence');
@@ -9,8 +9,11 @@ const del = require('del');
 const browserSync = require('browser-sync');
 const uglifyjs = require('uglify-js');
 const minifier = require('gulp-uglify/minifier');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
 const pump = require('pump');
 const stylus = require('gulp-stylus');
+const fs = require('fs');
 
 gulp.task('clean', () => {
   return del('./dist');
@@ -23,10 +26,34 @@ gulp.task('html', () => {
   ]);
 });
 
-gulp.task('scripts', () => {
+// Vendor file tips from: http://blog.revathskumar.com/2016/02/browserify-separate-app-and-vendor-bundles.html
+const vendors = Object.keys(JSON.parse(fs.readFileSync('package.json')).dependencies);
+
+gulp.task('scripts:vendor', () => {
+  const b = browserify({ debug: true });
+  vendors.forEach(lib => b.require(lib));
   return pump([
-    gulp.src(['./src/scripts/main.js']),
-    browserify({ debug: true }),
+    b.bundle(),
+    source('vendor.js'),
+    buffer(),
+    sourcemaps.init({ loadMaps: true }),
+    minifier({}, uglifyjs),
+    sourcemaps.write('.'),
+    gulp.dest('./dist/scripts/'),
+  ]);
+});
+
+
+gulp.task('scripts:app', () => {
+  return pump([
+    browserify({
+      entries: ['./src/scripts/main.js'],
+      debug: true,
+    })
+    .external(vendors)
+    .bundle(),
+    source('main.js'),
+    buffer(),
     sourcemaps.init({ loadMaps: true }),
     minifier({}, uglifyjs),
     sourcemaps.write('.'),
@@ -59,11 +86,11 @@ gulp.task('browser-sync', () => {
   });
 });
 
-gulp.task('default', sequence('clean', ['html', 'scripts', 'styles', 'images']));
+gulp.task('default', sequence('clean', ['html', 'scripts:app', 'scripts:vendor', 'styles', 'images']));
 
 gulp.task('dev', ['default', 'browser-sync'], () => {
   gulp.watch('./src/**/*.html', ['html', browserSync.reload]);
-  gulp.watch('./src/scripts/**/*', ['scripts', browserSync.reload]);
+  gulp.watch('./src/scripts/**/*', ['scripts:app', browserSync.reload]);
   gulp.watch('./src/styles/**/*', ['styles', browserSync.reload]);
   gulp.watch('./src/images/**/*', ['images', browserSync.reload]);
 });
