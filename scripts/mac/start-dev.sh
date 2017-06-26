93ff366a46db0d5547ad8aa070cda63c99bfd7a0
Josh Santangelo <josh@endquote.com>
2017-06-25 21:01:44 -0700

    set downloads folder in all scripts #8

--- scripts/mac/start-dev.sh
+++ scripts/mac/start-dev.sh
@@ -3,5 +3,5 @@ cd $DIR
 cd ../../
 npm install
 mongod --config /usr/local/etc/mongod.conf &
-npm --prefix server run dev -- --username=username --password=password & # --port=8080 --downloads=./downloads/ --database=mongodb://localhost/
+npm --prefix server start -- --downloads=~/Downloads/VideoGallery & # --username=username --password=password --port=8080 --downloads=./downloads/ --database=mongodb://localhost/
 npm --prefix client run dev && fg
--- scripts/mac/start-kiosk.sh
+++ scripts/mac/start-kiosk.sh
@@ -3,6 +3,6 @@ cd $DIR
 cd ../../
 npm install
 mongod --config /usr/local/etc/mongod.conf &
-npm --prefix server start -- & # --username=username --password=password --port=8080 --downloads=./downloads/ --database=mongodb://localhost/
+npm --prefix server start -- --downloads=~/Downloads/VideoGallery & # --username=username --password=password --port=8080 --downloads=./downloads/ --database=mongodb://localhost/
 sleep 10
 open -a "Google Chrome" --args --kiosk --app=http://localhost:8080/
