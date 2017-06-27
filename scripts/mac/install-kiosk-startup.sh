9499695b88caf8dde1a1b3517b0a60f06068e859
Josh Santangelo <josh@endquote.com>
2017-06-26 21:33:27 -0700

    try launchd again #1

--- scripts/mac/install-kiosk-startup.sh
+++ scripts/mac/install-kiosk-startup.sh
@@ -1,2 +1,3 @@
-# this doesn't work
-#(crontab -l 2>/dev/null; echo "@reboot ~/VideoGallery/node_modules/video-gallery/scripts/mac/start-kiosk.sh ") | crontab -
+mkdir -p ~/Library/LaunchAgents
+cp com.endquote.launch-videogallery.plist ~/Library/LaunchAgents/com.endquote.launch-videogallery.plist
+launchctl load -w ~/Library/LaunchAgents/com.endquote.launch-videogallery.plist
