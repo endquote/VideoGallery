b016350c9b19931e9bbde67b8f1b6f7f4f1fc216
Josh Santangelo <josh@endquote.com>
2017-06-26 21:02:47 -0700

    no args in scripts #14

--- scripts/mac/start-dev.sh
+++ scripts/mac/start-dev.sh
@@ -3,5 +3,5 @@ cd $DIR
 cd ../../
 npm install
 mongod --config /usr/local/etc/mongod.conf &
-npm --prefix server start -- --downloads=~/Downloads/VideoGallery & # --username=username --password=password --port=8080 --downloads=./downloads/ --database=mongodb://localhost/
+npm --prefix server start &
 npm --prefix client run dev && fg
