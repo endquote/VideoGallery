258bd3822f4093cf1a066d45b4ccb5f1f2ab8e52
Josh Santangelo <josh@endquote.com>
2017-06-26 22:02:31 -0700

    more fighting with startup #1

--- scripts/mac/install-kiosk-startup.sh
+++ scripts/mac/install-kiosk-startup.sh
@@ -1,3 +1,5 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 mkdir -p ~/Library/LaunchAgents
 cp com.endquote.launch-videogallery.plist ~/Library/LaunchAgents/com.endquote.launch-videogallery.plist
 launchctl load -w ~/Library/LaunchAgents/com.endquote.launch-videogallery.plist
