038a4ddeb994bb767720599cb784455542e663bb
Josh Santangelo <josh@endquote.com>
2017-07-04 12:13:48 -0700

    move client-url script

--- scripts/mac/start-browser.sh
+++ scripts/mac/start-browser.sh
@@ -2,6 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ../../
 export NODE_CONFIG_DIR=~/rgbtv
-export RGBTV_URL=`node client/src/scripts/client-url.js`
+export RGBTV_URL=`node server/client-url.js`
 sleep 30
 open -a "Google Chrome" --args --kiosk --app=$RGBTV_URL
