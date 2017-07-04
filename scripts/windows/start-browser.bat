5ed25f886772e3a4481ca0f1326d150de01a0bea
Josh Santangelo <josh@endquote.com>
2017-07-04 11:43:09 -0700

    use config file for URL on windows too

--- scripts/windows/start-browser.bat
+++ scripts/windows/start-browser.bat
@@ -2,6 +2,6 @@ SET DIR=%~dp0
 cd %DIR%
 cd ..\..\
 set NODE_CONFIG_DIR=C:\rgbtv
-SET RGBTV_URL=node client/src/scripts/client-url.js
+FOR /F "tokens=* USEBACKQ" %%F IN (`node client/src/scripts/client-url.js`) DO (SET RGBTV_URL=%%F)
 timeout 30
 "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" --kiosk --incognito --disable-pinch --overscroll-history-navigation=0 %RGBTV_URL%
