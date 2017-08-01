06f73cb7821116957304c885d089fe5e4f14db0c
Josh Santangelo <josh@endquote.com>
2017-08-01 09:35:52 -0700

    don't do verbose on video info

--- server/downloader.js
+++ server/downloader.js
@@ -24,7 +24,7 @@ class Downloader {
     this._addQueue = [];
 
     // https://github.com/rg3/youtube-dl/blob/master/README.md
-    this.infoCmd = "youtube-dl -v --dump-json --playlist-items 1";
+    this.infoCmd = "youtube-dl --dump-json --playlist-items 1";
     this.downloadCmd =
       "youtube-dl -v --write-thumbnail --no-progress --playlist-items 1 -o";
 
