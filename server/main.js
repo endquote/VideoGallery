eadbaac86fac7a257df749eb6dfd49c351b4df72
Josh Santangelo <josh@endquote.com>
2018-03-16 15:14:13 -0700

    nicer server startup

--- server/main.js
+++ server/main.js
@@ -5,15 +5,14 @@ const WebServer = require('./webServer');
 const SocketServer = require('./socketServer');
 const Downloader = require('./downloader');
 
-Database.init(config.get('database'))
-  .then(() => Downloader.init(config.get('downloads')))
-  .then(() =>
-    WebServer.init(
-      config.get('port'),
-      config.get('downloads'),
-      config.get('username'),
-      config.get('password'),
-    ),
-  )
-  .then(() => SocketServer.init())
-  .then(() => Database.cleanup());
+class Server {
+  static async init() {
+    await Database.init(config.get('database'));
+    await Downloader.init(config.get('downloads'));
+    await WebServer.init(config.get('port'), config.get('downloads'), config.get('username'), config.get('password'));
+    await SocketServer.init();
+    await Database.cleanup();
+  }
+}
+
+Server.init();
